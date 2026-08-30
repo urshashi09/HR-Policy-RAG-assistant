20e076d99d74b3e19dbf53de3f882877ee0035c9
urshashi09 <urshashimaj@gmail.com>
2026-08-30 14:24:14 +0530

    feat: add document text splitting

--- hr_assistant/doc_splitter.py
+++ hr_assistant/doc_splitter.py
@@ -1,1 +1,11 @@
+from langchain_text_splitters import RecursiveCharacterTextSplitter
 
+from hr_assistant import config
+
+def split_into_chunks(documents):
+    """split a list of langchain documents into chunks"""
+    text_splitter = RecursiveCharacterTextSplitter(
+        chunk_size=config.CHUNK_SIZE,
+        chunk_overlap=config.CHUNK_OVERLAP
+        )
+    return text_splitter.split_documents(documents)
